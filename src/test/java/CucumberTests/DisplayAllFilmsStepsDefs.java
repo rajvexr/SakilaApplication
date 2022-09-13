f4619ee23cbba8b889c41673dad659a113da4699
rajvexr <thers3001@gmail.com>
2022-09-13 09:03:42 +0100

    adding cucumber tests

---
+++ src/test/java/CucumberTests/DisplayAllFilmsStepsDefs.java
@@ -0,0 +1,25 @@
+package CucumberTests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DisplayAllFilmsStepsDefs {
+
+    @Given("the application is running")
+    public void the_application_is_running() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @When("the {string} loads")
+    public void the_loads(String pageName) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @When("the api connects")
+    public void the_api_connects() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("display all the films")
+    public void display_all_the_films() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+}
